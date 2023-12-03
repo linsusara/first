d8f3643f3daf8005a96a11f7ea9df99d1144019c
linsusara <152804497+linsusara@users.noreply.github.com>
2023-12-03 14:33:35 -0500

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  3 12:12:45 2023
+
+@author: ABRAHAM JOSEPH
+"""
+
+# File: your_app.py
+import streamlit as st
+
+def main():
+    st.title("My Streamlit App")
+    st.write("Hello, this is a basic Streamlit app.")
+
+if __name__ == "__main__":
+    main()
